6acda06657c51c4892cc779812503df195c33f36
yaman3108 <133077812+yaman3108@users.noreply.github.com>
2024-10-22 13:53:14 +0530

    Update EXP20.1.cpp

--- EXP20.1.cpp
+++ EXP20.1.cpp
@@ -1,46 +1,44 @@
 #include <iostream>
 using namespace std;
 
-void swap (int *a, int *b)
-{
-  int temp;
-  temp = *a;
-  *a = *b;
-  *b = temp;
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-void s_sort(int *a, int el)
-{
-  int n = 0;
-  int *b;
-
-  while (n != el)
-    {
-      b = a + 1;
-      for(int i = 0; i <(el-1) - n; i++)
-        {
-          if(*a>*b)
-          {
-            swap(a,b);
-          }
+void s_sort(int *a, int el) {
+    for (int i = 0; i < el - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < el; j++) {
+            if (a[j] < a[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            swap(&a[i], &a[minIndex]);
         }
     }
 }
-int main()
-{
-  int nel;
-  int a[nel];
-  cout<< "No. of elements: ";
-  cin>>nel;
-  for(int i = 0; i < nel; i++)
-    {
-      cin>>a[i];
+
+int main() {
+    int nel;
+    cout << "No. of elements: ";
+    cin >> nel;
+    int a[nel]; // Declare array after reading the size
+
+    cout << "Enter the elements: " << endl;
+    for (int i = 0; i < nel; i++) {
+        cin >> a[i];
     }
-  cout<<"Sorted array: "<<endl;
-  sort(&a[0], nel);
-  for(int i = 0; i < nel; i++)
-    {
-      cout<<a[i]<<" ";
+
+    s_sort(a, nel); // Sort the array
+
+    cout << "Sorted array: " << endl;
+    for (int i = 0; i < nel; i++) {
+        cout << a[i] << " ";
     }
-  return 0;
+    cout << endl;
+
+    return 0;
 }
